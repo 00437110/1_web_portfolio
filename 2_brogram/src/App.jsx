0db2e80fd81e6bc35393d102479bb0e48716fa40
Erick Domínguez <50849788+00437110@users.noreply.github.com>
2025-07-04 00:06:37 -0600

    First commit to Github

---
+++ 2_brogram/src/App.jsx
@@ -0,0 +1,21 @@
+import Grid from "./components/Grid"
+import Layout from "./components/Layout"
+
+
+function App() {
+
+  return (
+    <Layout>
+
+      <main>
+        <Grid/>
+
+
+      </main>
+
+    </Layout>
+
+  )
+}
+
+export default App
